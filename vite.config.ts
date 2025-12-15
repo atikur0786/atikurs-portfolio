2904d3508c4762953efd3d24d5cff060ed2428f0
Atikur Satter Mondal <atikursattermondal@gmail.com>
2025-12-15 15:37:43 +0530

    feat: Github pages deployment support added

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
     },
     plugins: [react()],
+    base: "/atikurs-portfolio/",
     define: {},
     resolve: {
       alias: {
